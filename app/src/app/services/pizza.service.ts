cd3c2c6f0f06a1fff8267017cfdfb2f18a45d6dc
Denesis Peralta <dcolmenares1901@gmail.com>
2022-06-27 09:57:32 +0200

    service

--- app/src/app/services/pizza.service.ts
+++ app/src/app/services/pizza.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PizzaInterface } from '../models/pizza.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PizzaService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getPizzas() {
+    return this.httpClient.get('http://localhost:3000/pizzas');
+  }
+
+  createPizza(newPizza: PizzaInterface) {
+    return this.httpClient.post('http://localhost:3000/pizzas', newPizza);
+  }
+
+  deletePizza(pizzaId: number) {
+    return this.httpClient.delete(`http://localhost:3000/pizzas/${pizzaId}`);
+  }
+
+  updatePizza(pizzaId: number, updatedPizza: PizzaInterface) {
+    return this.httpClient.delete(`http://localhost:3000/pizzas/${pizzaId}`);
+  }
 }
