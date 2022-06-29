8e2565e4ef3c34429995b61a54fe8426d080036c
Anny Pereira <annypereiras@gmail.com>
2022-06-29 14:58:40 +0200

    modified

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { GalleryComponent } from './pages/gallery/gallery.component';
-import { HomeComponent } from './pages/home/home.component';
-import { MenuComponent } from './pages/menu/menu.component';
+import { ShoppingCartComponent } from "./pages/shopping-cart/shopping-cart.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { GalleryComponent } from "./pages/gallery/gallery.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { MenuComponent } from "./pages/menu/menu.component";
 
 const routes: Routes = [
-  { path: 'menu', component: MenuComponent },
-  { path: 'gallery', component: GalleryComponent },
-  { path: 'shopping-cart', component: ShoppingCartComponent },
-  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: "menu", component: MenuComponent },
+  { path: "gallery", component: GalleryComponent },
+  { path: "shopping-cart", component: ShoppingCartComponent },
+  { path: "", pathMatch: "full", component: HomeComponent },
   //Pizzero que se le cae la pizza
   // {path: "*",  component: HomeComponent}
 ];
